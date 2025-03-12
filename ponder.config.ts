789e6d47d475240fe11bdd31d49ca845ab53cac6
kanvgupta <85568419+kanvgupta@users.noreply.github.com>
2025-03-12 18:23:33 +0530

    revert to using Base Mainnet

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,17 +4,17 @@ import { UniswapV3PoolABI } from "./abis/UniswapV3PoolABI";
 
 export default createConfig({
   networks: {
-    arbitrum: {
-      chainId: 42161,
-      transport: http(process.env.PONDER_RPC_URL_42161),
+    base: {
+      chainId: 8453,
+      transport: http(process.env.PONDER_RPC_URL_8453),
+      maxRequestsPerSecond: 300,
     },
   },
   contracts: {
     UniswapV3Pool: {
-      network: "arbitrum",
-      address: ["0xC6962004f452bE9203591991D15f6b388e09E8D0"],
-      startBlock: 188249317,
-      endBlock: 189999317,
+      network: "base",
+      address: ["0xd0b53D9277642d899DF5C87A3966A349A798F224"],
+      startBlock: 21620977,
       abi: UniswapV3PoolABI,
     },
   },
--- poolConfig.ts
+++ poolConfig.ts
@@ -1,34 +1,34 @@
 export interface PoolConfig {
+  address: string;
+  chainId: number;
+  isToken1Volatile: boolean;
+  token0: {
     address: string;
-    chainId: number;
-    isToken1Volatile: boolean;
-    token0: {
-      address: string;
-      decimals: number;
-      symbol: string;
-    };
-    token1: {
-      address: string;
-      decimals: number;
-      symbol: string;
-    };
-  }
-  
-  export const POOL_CONFIGS: Record<string, PoolConfig> = {
-    "0xc6962004f452be9203591991d15f6b388e09e8d0": {
-      address: "0xc6962004f452be9203591991d15f6b388e09e8d0",
-      chainId: 42161, // Arbitrum One
-      isToken1Volatile: false, // is WETH token1?
-      token0: {
-        address: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", // WETH on Arbitrum
-        decimals: 18,
-        symbol: "WETH"
-      },
-      token1: {
-        address: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", // USDC on Arbitrum
-        decimals: 6,
-        symbol: "USDC"
-      }
-    },
-    // Add other pools...
+    decimals: number;
+    symbol: string;
   };
+  token1: {
+    address: string;
+    decimals: number;
+    symbol: string;
+  };
+}
+
+export const POOL_CONFIGS: Record<string, PoolConfig> = {
+  "0xd0b53d9277642d899df5c87a3966a349a798f224": {
+    address: "0xd0b53d9277642d899df5c87a3966a349a798f224",
+    chainId: 8453, // Arbitrum One
+    isToken1Volatile: false, // is WETH token1?
+    token0: {
+      address: "0x4200000000000000000000000000000000000006", // WETH on Arbitrum
+      decimals: 18,
+      symbol: "WETH",
+    },
+    token1: {
+      address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", // USDC on Arbitrum
+      decimals: 6,
+      symbol: "USDC",
+    },
+  },
+  // Add other pools...
+};
