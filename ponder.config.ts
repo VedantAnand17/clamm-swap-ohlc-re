712b681cfd17bf3986b70e9d0f80a9f7f67e9b85
kanvgupta <85568419+kanvgupta@users.noreply.github.com>
2025-03-12 18:11:51 +0530

    Revert "deployment commit"
    This reverts commit 6a36ac7a68e2c6faf7b89f0772782e7fcb57fd73.

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,16 +4,17 @@ import { UniswapV3PoolABI } from "./abis/UniswapV3PoolABI";
 
 export default createConfig({
   networks: {
-    tenderly: {
-      chainId: 8450,
-      transport: http(process.env.PONDER_RPC_URL_8450),
+    arbitrum: {
+      chainId: 42161,
+      transport: http(process.env.PONDER_RPC_URL_42161),
     },
   },
   contracts: {
     UniswapV3Pool: {
-      network: "tenderly",
-      address: ["0xd0b53D9277642d899DF5C87A3966A349A798F224"],
-      startBlock: 21495365,
+      network: "arbitrum",
+      address: ["0xC6962004f452bE9203591991D15f6b388e09E8D0"],
+      startBlock: 188249317,
+      endBlock: 189999317,
       abi: UniswapV3PoolABI,
     },
   },
